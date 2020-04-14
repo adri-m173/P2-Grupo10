40833ca4c4b5b49bd639089871283e41eac9cef6
Adrián Muñoz <adrian.in2448@gmail.com>
2020-04-14 16:01:43 +0200

    Retocado el método de hacerLogin
    Arreglado el problema con el return del método

--- Sistema.java
+++ Sistema.java
@@ -1,26 +1,30 @@
 package com.p2;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Sistema {
+public class Sistema implements Serializable {
     private ArrayList<Usuario> usuarios;
 
     public void registrarUsuario(String nick_, String nombre_, String apellidos_, String pass_, String email_){
         Usuario nuevo = new Usuario(nick_, nombre_, apellidos_, pass_, email_);
         usuarios.add(nuevo);
+        System.out.println("Usuario creado correctamente");
     }
 
     public boolean hacerLogin(String nick_, String pass_){
+        boolean salida = false;
         if (usuarios == null || usuarios.size()<=0){
             System.out.println("No existen usuarios registrados");
-            return false;
+            salida = false;
         }
         for (Usuario usr: usuarios) {
             if (usr.getNick().equals(nick_) && usr.getPass().equals(pass_)) {
-                return true;
+                salida = true;
             } else {
-                return false;
+                salida = false;
             }
         }
+        return salida;
     }
 }
