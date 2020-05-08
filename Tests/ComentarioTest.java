f16538e84d77d91e0f4ae3b9b634bda2d42d44a8
Adrián Muñoz <adrian.in2448@gmail.com>
2020-05-08 17:02:38 +0200

    Comentadas las clases hechas hasta ahora

--- Tests/ComentarioTest.java
+++ Tests/ComentarioTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ComentarioTest {
+    //Se crean los objetos necesarios para los tests
     Usuario usuario1 = new Usuario("nick1", "nombre1", "apellido1", "pass1", "email1");
     Usuario usuario2 = new Usuario("nick2", "nombre2", "apellido2", "pass2", "email2");
     Comentario comentario1 = new Comentario(usuario1,"comentario1");
@@ -12,25 +13,25 @@ public class ComentarioTest {
 
     @Test
     public void votarPositivamente() {
-        comentario1.votarPositivamente(usuario2);
-        assertEquals(1, comentario1.getPuntuacion());
+        comentario1.votarPositivamente(usuario2); //El usuario2 vota positivamente el comentario1 del usuario1
+        assertEquals(1, comentario1.getPuntuacion()); //Se comprueba que la puntuacion del comentario1 es ahora igual a 1
     }
 
     @Test
     public void votarNegativamente() {
-        comentario2.votarNegativamente(usuario1);
-        assertEquals(-1, comentario2.getPuntuacion());
+        comentario2.votarNegativamente(usuario1); //El usuario1 vota negativamente el comentario2 del usuario2
+        assertEquals(-1, comentario2.getPuntuacion()); //Se comprueba que la puntuacion del comentario2 es ahora igual a -1
     }
 
     @Test
     public void votarComentarioPropio() {
-        comentario1.votarPositivamente(usuario1);
-        assertEquals(0, comentario1.getPuntuacion());
-        comentario1.votarNegativamente(usuario1);
-        assertEquals(0, comentario1.getPuntuacion());
-        comentario2.votarPositivamente(usuario2);
-        assertEquals(0, comentario2.getPuntuacion());
-        comentario2.votarNegativamente(usuario2);
-        assertEquals(0, comentario2.getPuntuacion());
+        comentario1.votarPositivamente(usuario1); //El usuario1 intenta votar positivamente su propio comentario
+        assertEquals(0, comentario1.getPuntuacion()); //Se comprueba que la puntuacion del comentario del usuario1 no ha variado
+        comentario1.votarNegativamente(usuario1); //El usuario1 intenta votar negativamente su propio comentario
+        assertEquals(0, comentario1.getPuntuacion()); //Se comprueba que la puntuacion del comentario del usuario1 no ha variado
+        comentario2.votarPositivamente(usuario2); //El usuario2 intenta votar positivamente su propio comentario
+        assertEquals(0, comentario2.getPuntuacion()); //Se comprueba que la puntuacion del comentario del usuario2 no ha variado
+        comentario2.votarNegativamente(usuario2); //El usuario2 intenta votar negativamente su propio comentario
+        assertEquals(0, comentario2.getPuntuacion()); //Se comprueba que la puntuacion del comentario del usuario2 no ha variado
     }
 }
--- Tests/UsuarioTest.java
+++ Tests/UsuarioTest.java
@@ -5,41 +5,42 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class UsuarioTest {
+    //Se crean los objetos y variables necesarios para los tests
     Administrador admin = new Administrador("nick", "nombre", "apellidos", "pass", "email");
     Usuario usuario = new Usuario("nick", "nombre", "apellido", "pass", "email");
     private final int dias = 5;
 
     @Test
     public void avanzarDias() {
-        admin.banear(usuario, dias);
-        usuario.avanzarDias(1);
-        assertEquals(dias - 1, usuario.getDiasBaneado());
+        admin.banear(usuario, dias); //Se banea al usuario 5 dias
+        usuario.avanzarDias(1); //Se avanza 1 dia en el contador de dias restantes del baneo
+        assertEquals(dias - 1, usuario.getDiasBaneado()); //Se comprueba que los dias restantes de baneo es 1 dia menor al inicial
     }
 
     @Test
     public void UsuarioBaneado() {
-        admin.banear(usuario, dias);
-        assertTrue(usuario.comprobarbaneo(usuario));
-        assertEquals(dias, usuario.getDiasBaneado());
+        admin.banear(usuario, dias); //Se banea al usuario 5 dias
+        assertTrue(usuario.comprobarbaneo(usuario)); //Se comprueba que la condicion booleana de si un usuario esta baneado esta a True
+        assertEquals(dias, usuario.getDiasBaneado()); //Se comprueba que la variable que guarda los dias que ese usuario esta baneado guarda el numero correcto
     }
 
     @Test
     public void UsuarioDesBaneado() {
-        admin.desbanear(usuario);
-        assertFalse(usuario.comprobarbaneo(usuario));
+        admin.desbanear(usuario); //Se desbanea al usuario
+        assertFalse(usuario.comprobarbaneo(usuario)); //Se comprueba que la condicion booleana de si un usuario esta baneado esta a False
     }
 
     @Test
-    public void comprobarnick() {
+    public void comprobarnick() { //Se comprueba que el sistema reconoce si un nick ya esta en uso por otro usuario
         assertTrue(usuario.comprobarnick("nick", usuario));
         assertFalse(usuario.comprobarnick("nick2", usuario));
     }
 
     @Test
     public void recibirNotificacion() {
-        usuario.recibirNotificacion("Notificacion");
-        assertFalse(usuario.getNotificaciones().isEmpty());
-        usuario.recibirNotificacion("Notificacion 2");
-        assertEquals(2, usuario.getNotificaciones().size());
+        usuario.recibirNotificacion("Notificacion"); //El usuario recibe una notificacion
+        assertFalse(usuario.getNotificaciones().isEmpty()); //Se comprueba que el ArrayList que guarda las notificaciones de un usuario ya no esta vacio
+        usuario.recibirNotificacion("Notificacion 2"); //El usuario recibe otra notificacion
+        assertEquals(2, usuario.getNotificaciones().size()); //Se comprueba que el ArrayList de las notificaciones ahora tiene tamanio 2
     }
 }
