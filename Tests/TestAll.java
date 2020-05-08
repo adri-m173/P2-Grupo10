4ffb29e0982097fcb03ba7fcf848e15b871f3c17
Antonio Cuadrado <antoniocuad15@gmail.com>
2020-05-08 18:24:54 +0200

    Añadido test clase Administrador

--- Tests/TestAll.java
+++ Tests/TestAll.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({UsuarioTest.class, ComentarioTest.class, EntradaTest.class})
+@SuiteClasses({UsuarioTest.class, ComentarioTest.class, EntradaTest.class, AdministradorTest.class})
 
 public class TestAll {
 }
