b448287b523929d766e3c30b5f37a597c36dbf38
Adrián Muñoz <adrian.in2448@gmail.com>
2020-05-11 14:18:27 +0200

    Añadido el test SubforoTest (revisar)
    Se ha añadido la clase SubforoTest, pero se deben revisar 3 test que no pasan

---
+++ Tests/SubforoTest.java
@@ -0,0 +1,44 @@
+package com.p3;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SubforoTest {
+    Usuario usuario = new Usuario("nick", "nombre", "apellidos", "contra", "email");
+    Subforo subforo = new Subforo("titulo");
+    Entrada entrada = new Entrada(usuario, "titulo", "contenido", 0);
+
+    @Test
+    public void aniadirEntrada() {
+        int tamanioEsperado = subforo.getEntradas().size() + 1; //Se guarda el tamanio esperado del ArrayList que guarda las entradas del subforo
+        subforo.aniadirEntrada(entrada); //Se aniade la entrada al subforo
+        assertEquals(tamanioEsperado, subforo.getEntradas().size()); //Se comprueba que el ArrayList que guarda las entradas que tiene el subforo ahora tiene una entrada mas
+    }
+
+    @Test
+    public void entradaMasVotada() { //REVISAR
+        assertEquals(subforo.EntradaMasVotada().getTitulo(), entrada.getTitulo());
+    }
+
+    @Test
+    public void aniadirSubscriptor() {
+        int tamanioEsperado = subforo.getUsuariosSubscritos().size() + 1; //Se guarda el tamanio del ArrayList esperado en la variable tamanioEsperado
+        subforo.aniadirSubscriptor(usuario); //El usuario se subscribe al subforo
+        assertEquals(tamanioEsperado, subforo.getUsuariosSubscritos().size()); //Se comprueba que el ArrayList que guarda los subscriptores del subforo ahora tiene un subscriptor mas
+    }
+
+    @Test
+    public void notificar() { //REVISAR
+        int tamanioEsperado = usuario.getNotificaciones().size() + 1; //Se guarda el tamanio esperado del ArrayList que guarda las notificaciones de un usuario
+        subforo.notificar(); //Se notifica a los usuarios subscritos al subforo
+        assertEquals(tamanioEsperado, usuario.getNotificaciones().size()); //Se comprueba que el ArrayList que guarda las notificaciones del usuario subscrito ahora tiene una notificacion mas
+    }
+
+    @Test
+    public void eliminarSubscriptor() { //REVISAR
+        int tamanioEsperado = subforo.getUsuariosSubscritos().size() - 1; //Se guarda el tamanio del ArrayList esperado en la variable tamanioEsperado
+        subforo.eliminarSubscriptor(usuario); //El usuario se da de baja del subforo
+        assertEquals(tamanioEsperado, subforo.getUsuariosSubscritos().size()); //Se comprueba que el ArrayList que guarda los subscriptores del subforo ahora tiene un subscriptor menos
+    }
+}
