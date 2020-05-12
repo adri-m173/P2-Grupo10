3dd868e680551e3abab0743c0f9f0c917ba02f67
Rodrigo Marqués <r.marques.2018@alumnos.urjc.es>
2020-05-12 23:50:09 +0200

    Solucionado problema HacerLoginTest
    Se ha solucionado el problema a revisar de HacerLogInTest y ahora funciona correctamente. Además se han añadido varios metodos a la misma clase. Se ha detectado un nuevo error a revisar en vetarEntrada

--- Tests/SistemaTest.java
+++ Tests/SistemaTest.java
@@ -1,5 +1,4 @@
 package com.p3;
-
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -17,40 +16,62 @@ public class SistemaTest {
 
     @Test
     public void registrarAdmin() {
-        int adminAniadido = sistema.getUsuarios().size() + 1;
+        int adminAniadido = sistema.getUsuarios().size();
         sistema.registrarAdmin("nick", "nombre", "apellidos", "contra", "admin@email.com"); //Intentamos registrar un nuevo administrador 1 en el sistema con correo no valido
-        assertNotEquals(adminAniadido, sistema.getUsuarios().size()); //Comprobamos que el administrador 1 no se ha añadido a la lista de usuarios del sistema por no tener un correo válido
+        assertEquals(adminAniadido, sistema.getUsuarios().size()); //Comprobamos que el administrador 1 no se ha añadido a la lista de usuarios del sistema por no tener un correo válido
         sistema.registrarAdmin("nick", "nombre", "apellidos", "contra", "email@urjc.es"); //Registramos un nuevo administrador 2 en el sistema con un correo de profesor
-        assertEquals(adminAniadido, sistema.getUsuarios().size()); //Comprobamos que el administrador 2 se ha añadido a la lista de usuarios del sistema por tener un correo de profesor valido
-        sistema.registrarAdmin("nick1", "nombre", "apellidos", "contra", "email@alumnos.urjc.es"); //Registramos un nuevo administrador 3 en el sistema con un correo de alumno
-        assertEquals(adminAniadido + 1, sistema.getUsuarios().size()); //Comprobamos que el administrador 3 se ha añadido a la lista de usuarios del sistema al tener un correo de alumno valido
+        assertEquals(adminAniadido + 1, sistema.getUsuarios().size()); //Comprobamos que el administrador 2 se ha añadido a la lista de usuarios del sistema por tener un correo de profesor valido
+        sistema.registrarAdmin("nick2", "nombre", "apellidos", "contra", "email@alumnos.urjc.es"); //Registramos un nuevo administrador 3 en el sistema con un correo de alumno
+        assertEquals(adminAniadido + 2, sistema.getUsuarios().size()); //Comprobamos que el administrador 3 se ha añadido a la lista de usuarios del sistema al tener un correo de alumno valido
         sistema.registrarAdmin("nick", "nombre", "apellidos", "contra", "email@urjc.es"); //Intentamos registrar un nuevo administrador 4 con datos ya registrados en el sistema
-        assertEquals(adminAniadido + 1, sistema.getUsuarios().size()); //Comprobamos que la lista de usuarios del sistema no ha aumentado su tamaño y por tanto no se ha añadido el administrador 4
+        assertEquals(adminAniadido + 2, sistema.getUsuarios().size()); //Comprobamos que la lista de usuarios del sistema no ha aumentado su tamaño y por tanto no se ha añadido el administrador 4
     }
 
     @Test
     public void registrarUsuario() {
-        int usuarioAniadido = sistema.getUsuarios().size() + 1;
+        int usuarioAniadido = sistema.getUsuarios().size();
         sistema.registrarUsuario("nick", "nombre", "apellidos", "contra", "admin@email.com"); //Intentamos registrar un nuevo usuario 1 en el sistema con correo no valido
-        assertNotEquals(usuarioAniadido, sistema.getUsuarios().size()); //Comprobamos que el usuario 1 no se ha añadido a la lista de usuarios del sistema por no tener un correo válido
-        sistema.registrarUsuario("nick", "nombre", "apellidos", "contra", "email@urjc.es"); //Registramos un nuevo usuario 2 en el sistema con un correo de profesor
-        assertEquals(usuarioAniadido, sistema.getUsuarios().size()); //Comprobamos que el usuario 2 se ha añadido a la lista de usuarios del sistema por tener un correo de profesor valido
-        sistema.registrarUsuario("nick1", "nombre", "apellidos", "contra", "email@alumnos.urjc.es"); //Registramos un nuevo usuario 3 en el sistema con un correo de alumno
-        assertEquals(usuarioAniadido + 1, sistema.getUsuarios().size()); //Comprobamos que el usuario 3 se ha añadido a la lista de usuarios del sistema al tener un correo de alumno valido
+        assertEquals(usuarioAniadido, sistema.getUsuarios().size()); //Comprobamos que el usuario 1 no se ha añadido a la lista de usuarios del sistema por no tener un correo válido
+        sistema.registrarUsuario("nick2", "nombre", "apellidos", "contra", "email@urjc.es"); //Registramos un nuevo usuario 2 en el sistema con un correo de profesor
+        assertEquals(usuarioAniadido + 1, sistema.getUsuarios().size()); //Comprobamos que el usuario 2 se ha añadido a la lista de usuarios del sistema por tener un correo de profesor valido
+        sistema.registrarUsuario("nick4", "nombre", "apellidos", "contra", "email@alumnos.urjc.es"); //Registramos un nuevo usuario 3 en el sistema con un correo de alumno
+        assertEquals(usuarioAniadido + 2, sistema.getUsuarios().size()); //Comprobamos que el usuario 3 se ha añadido a la lista de usuarios del sistema al tener un correo de alumno valido
         sistema.registrarUsuario("nick", "nombre", "apellidos", "contra", "email@urjc.es"); //Intentamos registrar un nuevo usuario 4 con datos ya registrados en el sistema
-        assertEquals(usuarioAniadido + 1, sistema.getUsuarios().size()); //Comprobamos que la lista de usuarios del sistema no ha aumentado su tamaño y por tanto no se ha añadido el usuario 4
+        assertEquals(usuarioAniadido + 2, sistema.getUsuarios().size()); //Comprobamos que la lista de usuarios del sistema no ha aumentado su tamaño y por tanto no se ha añadido el usuario 4
     }
 
     @Test
     public void hacerLogin() {
+        Usuario u = sistema.registrarUsuario("nick7", "nombre", "apellidos", "contra","email2@urjc.es");
+        boolean logeado = sistema.hacerLogin("nick7", "contra");
+        assertEquals(sistema.getUsuarioConectado(),u);
+        assertEquals(logeado,true);
     }
 
     @Test
     public void hacerLogout() {
+        sistema.hacerLogout();
+        assertEquals(sistema.getSesionIniciada(),false);
+        assertEquals(sistema.getUsuarioConectado(),null);
     }
 
     @Test
     public void comprobarusuario() {
+        System.out.println("desde aqui");
+        sistema.registrarUsuario("nick8", "nombre", "apellidos", "contra", "profe@urjc.es");
+        sistema.hacerLogin("nick8", "contra"); //sesion iniciada con un usuario profesor
+        Usuario u = sistema.getUsuarioConectado();
+        sistema.iniciarSubforo(sistema.getUsuarioConectado(), "titulo"); //el profesor crea un subforo
+        sistema.crearTextoPlano(sistema.getUsuarioConectado(), "titulo prueba", "contenido", 0); // el profesor añade una entrada
+        sistema.hacerLogout();
+        sistema.registrarAdmin("nick9", "nombre", "apellidos", "contra", "admin@urjc.es");
+        sistema.hacerLogin("nick9", "contra"); //un administrador inicia sesion
+        sistema.vetarEntradas((Administrador) sistema.getUsuarioConectado(), 5); //el administrador veta por 5 dias a un usuario
+        sistema.vetarEntradas((Administrador) sistema.getUsuarioConectado(), 5); //por algun motivo ya hay una entrada creada, asi que hay que utilizarlo dos veces
+        sistema.hacerLogout();
+        assertEquals(sistema.comprobarusuario("nick8"),false); //el usuario está correctamente baneado
+        u.avanzarDias(6);
+        assertEquals(sistema.comprobarusuario("nick8"),true);//han pasado los dias y el usuario ya no está baneado
     }
 
     @Test
